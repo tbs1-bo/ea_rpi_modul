525d7af9fab6f25e78a34c18eadd6281fb9f7a00
Marco Bakera <Marco@Bakera.de>
2016-07-13 18:02:59 +0200

    Pin-Belegungen werden zusätlich ausgegeben

--- RPiDummy/GPIO.py
+++ RPiDummy/GPIO.py
@@ -10,6 +10,14 @@ PUD_DOWN = 4
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+PINS = {}
+
+def __alle_pins_ausgeben():
+    s = "PIN-BELEGUNGEN: "
+    for p in PINS:        
+        s += "{pin}:{on} ".format(pin=p, on=PINS[p])
+    log.debug(s)
+
 def input(pin):
     """Dummy Methode, die zufällig True oder False zurückgibt."""
     log.info("Input für pin " + str(pin))
@@ -18,18 +26,27 @@ def input(pin):
     else:
         return False
         
-        
 def setmode(board):    
     """Macht nichts."""
     log.info("Setze boardmode auf " + str(board))
 
-
 def setup(pin, in_out):
     """Macht nichts."""    
+    global PINS
     log.info("Setup pin {p} modus {m}".format(p=pin, m=in_out))
+    if type(pin) is list:
+        for p in pin:
+            setup(p, in_out)
+        return
+
+    PINS[pin] = False
 
 def output(pin, an_aus):
+    global PINS
     log.info("Output {a} an pin {p}".format(a=an_aus, p=pin))
+    PINS[pin] = an_aus
+    __alle_pins_ausgeben()
 
 def cleanup():
     log.info("cleanup")
+
