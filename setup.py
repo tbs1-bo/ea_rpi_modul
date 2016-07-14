6afe2fba6151537eb12c795394b3c7f987bb6c94
Marco Bakera <Marco@Bakera.de>
2016-07-14 12:19:16 +0200

    metadaten hinzu

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ import setuptools
 # https://packaging.python.org/distributing/
 setuptools.setup(
     name="eapi",
-    version="0.1.1",
+    url="https://github.com/pintman/ea_rpi_modul",
+    version="0.1.3",
     description="Modul zur Ansteuerung eines EA-Moduls fuer den Raspberry Pi.",
+    long_description="Ein Modul zu Ansteuerung eines Eingabe-Ausgabe-Moduls für den Raspberry. Für die Ausgabe dienen LEDs und für die Eingabe Taster. Es kommt im Bildungsbereich zum Einsatz.",
     author="Marco Bakera",
+    author_email="bakera@tbs1.de",
     packages=setuptools.find_packages())
 
 
-# Mit dem folgenden Befehl kann ein neues Release veröffentlich werden
+# Ein Release kann wie folgt erstellt werden
+# python3 setup.py sdist
+
+# Mit dem folgenden Befehl kann ein neues Release veröffentlicht werden
 # python3 setup.py sdist upload
