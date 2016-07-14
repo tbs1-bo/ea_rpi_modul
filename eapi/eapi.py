8ed2a6167a91afa8da5f7121da8810556b0f7f74
Marco Bakera <Marco@Bakera.de>
2016-07-14 11:52:48 +0200

    Testcode in eigene Datei ausgelagert.

--- eapi/eapi.py
+++ eapi/eapi.py
@@ -59,19 +59,3 @@ class EAModul:
         """Setzt alle Pins des Pi wieder in den Ausgangszustand."""
         GPIO.cleanup()
 
-
-if __name__ == "__main__":
-    ea_modul = EAModul(1, 2, 3, 4, 5)
-
-    ea_modul.schalte_led(1, False)
-    try:
-        for i in range(5):
-            ea_modul.schalte_led(0, True)
-            if ea_modul.taster_gedrueckt(0):
-                ea_modul.schalte_led(0, False)
-
-    except KeyboardInterrupt:
-        ea_modul.cleanup()
-    finally:
-        ea_modul.cleanup()
-
