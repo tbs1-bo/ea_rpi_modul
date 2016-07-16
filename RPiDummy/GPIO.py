2ea586e0718748e5f5dc95ba2c320221434839c3
Marco Bakera <Marco@Bakera.de>
2016-07-16 12:53:37 +0200

    event listener

--- RPiDummy/GPIO.py
+++ RPiDummy/GPIO.py
@@ -6,16 +6,17 @@ BOARD = 1
 IN = 2
 OUT = 3
 PUD_DOWN = 4
+BOTH = 5
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-PINS = {}
+__PINS = {}
 
 def __alle_pins_ausgeben():
     s = "PIN-BELEGUNGEN: "
-    for p in PINS:        
-        s += "{pin}:{on} ".format(pin=p, on=PINS[p])
+    for p in __PINS:        
+        s += "{pin}:{on} ".format(pin=p, on=__PINS[p])
     log.debug(s)
 
 def input(pin):
@@ -32,21 +33,27 @@ def setmode(board):
 
 def setup(pin, in_out):
     """Macht nichts."""    
-    global PINS
+    global __PINS
     log.info("Setup pin {p} modus {m}".format(p=pin, m=in_out))
     if type(pin) is list:
         for p in pin:
             setup(p, in_out)
         return
 
-    PINS[pin] = False
+    __PINS[pin] = False
 
 def output(pin, an_aus):
-    global PINS
+    global __PINS
     log.info("Output {a} an pin {p}".format(a=an_aus, p=pin))
-    PINS[pin] = an_aus
+    __PINS[pin] = an_aus
     __alle_pins_ausgeben()
 
 def cleanup():
     log.info("cleanup")
+
+def add_event_detect(pin, flanke):
+    log.info("Event registrieren für Pin " + str(pin))
+
+def add_event_callback(pin, methode):
+    log.info("Registriere Callback Methode {m} für Pin {p}".format(m=methode, p=pin))
 
