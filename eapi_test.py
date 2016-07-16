eb9515403a39c4f83151ef2b205c9c7dd3e91a9a
Marco Bakera <Marco@Bakera.de>
2016-07-16 12:16:50 +0200

    alte Kommentare entfernt

--- eapi_test.py
+++ eapi_test.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 if __name__ == "__main__":
     # 6 PINs unten links auf dem Pi verwenden. 1 PIN oben links für 3,3V.
     # 2 Taster, 3 LEDs und 1 GND
-    #ea_modul = EAModul(29, 31, 33, 35, 37)    
+    #ea_modul = EAModul(29, 31, 33, 35, 37)
     ea_modul = EAModul()
 
     def taster0_gedrueckt(pin):
@@ -16,12 +16,12 @@ if __name__ == "__main__":
 
     try:
         while not ea_modul.taster_gedrueckt(1):
-            ea_modul.schalte_led(EAModul.LED_ROT, True) # 0 =rot, 1=gelb, 2=grün
+            ea_modul.schalte_led(EAModul.LED_ROT, True)
             time.sleep(0.2)
             ea_modul.schalte_led(EAModul.LED_ROT, False)
             time.sleep(0.2)
 
-            ea_modul.schalte_led(EAModul.LED_GRUEN, True) # 0 =rot, 1=gelb, 2=grün
+            ea_modul.schalte_led(EAModul.LED_GRUEN, True)
             time.sleep(0.5)
             ea_modul.schalte_led(EAModul.LED_GRUEN, False)
             time.sleep(0.2)
