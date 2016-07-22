55a93bf0c2f25a86d25b4a9acf7fe73b2ea57d4f
Marco Bakera <Marco@Bakera.de>
2016-07-22 13:55:46 +0200

    Typo

--- eapi/__init__.py
+++ eapi/__init__.py
@@ -2,4 +2,4 @@
 Eingabe-Ausgabe-Moduls für den Raspberry Pi.
 
 Es besteht aus verschiedenen Untermodulen. Schau in die Dokumentation der
-jeweiligen Untermodule, um mehr über die Funktioneweise zu erfahren. """
+jeweiligen Untermodule, um mehr über die Funktionsweise zu erfahren. """
