2483d648fed5b804a53341e7849631fbd41cdb34
Marco Bakera <Marco@Bakera.de>
2016-08-08 20:26:21 +0200

    doctests aufgenommen

--- relase.sh
+++ relase.sh
@@ -2,18 +2,21 @@
 
 echo Tests laufen lassen
 python3 setup.py test
-
-echo Tests OK?
+echo Unit Tests OK?
 read
+
+for f in net.py hw.py;
+do
+	python3 -m doctest eapi/$f
+	echo Doctests $f ok?
+	read
+done
 
 echo Release erstellen und hochladen
 python3 setup.py sdist upload
-
 echo Release OK?
 read
 
-echo Dokumentation erstellen
+echo Dokumentation erstellen und hochladen
 doxygen
-
-echo Dokumentation hochladen
 python3 setup.py upload_docs
