05a722312e73b573d9d2bd3f0e03e3ff9bd2b208
Marco Bakera <Marco@Bakera.de>
2016-08-08 10:41:57 +0200

    Versionsstring in Modul ausgelagert.
    Kann nun auch im Quelltext und der Doku eingesehen werden. #7

--- eapi/__init__.py
+++ eapi/__init__.py
@@ -3,3 +3,5 @@ Eingabe-Ausgabe-Moduls für den Raspberry Pi.
 
 Es besteht aus verschiedenen Untermodulen. Schau in die Dokumentation der
 jeweiligen Untermodule, um mehr über die Funktionsweise zu erfahren. """
+
+VERSION = "0.3.5"
