c99ab94fd7b56d41a76d2914214a98521e2fc2d2
Marco Bakera <Marco@Bakera.de>
2016-08-13 15:10:52 +0200

    Buttons entfernt, Demoprogramm hinzu,

--- eapi/gui.py
+++ eapi/gui.py
@@ -16,7 +16,7 @@ Nun kann eine GUI für das EA-Modul erstellt werden.
 
 """
 
-from tkinter import Tk, Button, Label, StringVar
+from tkinter import *  # Tk, Button, Label, StringVar
 from eapi.hw import EAModul
 
 
@@ -39,7 +39,10 @@ class EAModulGui:
         # gui init
         fenster = Tk()
         fenster.title("EAModul - GUI")
+        fenster.geometry('300x300')
 
+        # TODO entfernen, wenn nicht mehr gebraucht
+        """
         btn_taster0 = Button(fenster, text="Taster 0",
                              command=self.__taster0_gedrueckt)
         btn_taster0.pack()
@@ -47,19 +50,24 @@ class EAModulGui:
         btn_taster1 = Button(fenster, text="Taster 1",
                              command=self.__taster1_gedrueckt)
         btn_taster1.pack()
+        """
 
         # LEDs erzeugen
+        # TODO Icons statt Text verwenden
         self.var_rot = StringVar(value="0")
-        lbl_led_rot = Label(fenster, textvariable=self.var_rot, bg='red')
-        lbl_led_rot.pack()
+        self.lbl_led_rot = Label(fenster, textvariable=self.var_rot,
+                                 bg='lightgrey')
+        self.lbl_led_rot.pack(expand=YES, fill=BOTH)
 
         self.var_gelb = StringVar(value="0")
-        lbl_led_gelb = Label(fenster, textvariable=self.var_gelb, bg='yellow')
-        lbl_led_gelb.pack()
+        self.lbl_led_gelb = Label(fenster, textvariable=self.var_gelb,
+                                  bg='lightgrey')
+        self.lbl_led_gelb.pack(expand=YES, fill=BOTH)
 
         self.var_gruen = StringVar(value="0")
-        lbl_led_gruen = Label(fenster, textvariable=self.var_gruen, bg='green')
-        lbl_led_gruen.pack()
+        self.lbl_led_gruen = Label(fenster, textvariable=self.var_gruen,
+                                   bg='lightgrey')
+        self.lbl_led_gruen.pack(expand=YES, fill=BOTH)
 
         fenster.mainloop()
 
@@ -69,18 +77,60 @@ class EAModulGui:
     def __taster1_gedrueckt(self):
         self.ea.schalte_led(EAModul.LED_ROT, False)
 
+    def __farbe_fuer_ledwert(self, led_wert, default_wert):
+        """Bestimmt für den led_wert eine Farbe.
+
+        Wenn der LED-Wert 1 ist, wird der default_wert zurückgegeben, sonst wird
+        lightgrey als Farbe verwendet."""
+
+        if led_wert == 1:
+            return default_wert
+        else:
+            return "lightgrey"
+
     def __rote_led_update(self, neuer_wert):
         self.var_rot.set(neuer_wert)
+        self.lbl_led_rot.configure(bg=self.__farbe_fuer_ledwert(neuer_wert,
+                                                                "red"))
 
     def __gelbe_led_update(self, neuer_wert):
         self.var_gelb.set(neuer_wert)
+        self.lbl_led_gelb.configure(bg=self.__farbe_fuer_ledwert(neuer_wert,
+                                                                 "yellow"))
 
     def __gruene_led_update(self, neuer_wert):
         self.var_gruen.set(neuer_wert)
+        self.lbl_led_gruen.configure(bg=self.__farbe_fuer_ledwert(neuer_wert,
+                                                                  "green"))
+
+
+def demo_taster():
+    """
+    Über den Taster 0 an dem Modul kann die gelbe LED auf dem Board und
+    gleichzeitig in der GUI geschaltet werden.
+    """
+    input(str(demo_taster.__doc__) + "\n(Enter für Start)")
+
+    def taster0_gedrueckt(_):
+        global ea
+        ea.schalte_led(EAModul.LED_GELB, ea.taster_gedrueckt(0))
+
+    ea = EAModul()
+    ea.taster_event_registrieren(0, taster0_gedrueckt)
+
+    # GUI startet und blockiert bis zum Ende
+    EAModulGui(ea)
+
+    ea.cleanup()
 
 
 def main():
-    EAModulGui(EAModul())
+    """
+    Hauptfunktion, die bei Start des Moduls ausgeführt wird.
+    """
+    demo = input("Welches Demo soll gestartet werden: demo_taster ")
+    if demo == "demo_taster":
+        demo_taster()
 
 
 if __name__ == '__main__':
