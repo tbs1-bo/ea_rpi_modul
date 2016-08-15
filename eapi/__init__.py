3f69c88d6353f21acf7d08ed0723bd2491270bef
Marco Bakera <marco@bakera.de>
2016-08-15 11:52:04 +0200

    Test Release.

--- eapi/__init__.py
+++ eapi/__init__.py
@@ -4,4 +4,4 @@ Eingabe-Ausgabe-Moduls für den Raspberry Pi.
 Es besteht aus verschiedenen Untermodulen. Schau in die Dokumentation der
 jeweiligen Untermodule, um mehr über die Funktionsweise zu erfahren. """
 
-VERSION = "0.4.2"
+VERSION = "0.4.2a"
