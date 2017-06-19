f9681be443251639805bdc70abcb0e57606006e2
Marco Bakera <marco@bakera.de>
2017-06-19 18:57:20 +0200

    new version.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import eapi
 
 # gemäß der Beschreibung von
 # https://packaging.python.org/distributing/
-# Weitere Hinweise stehen in dem Buch "Expert Python Programming" in 
+# Weitere Hinweise stehen in dem Buch "Expert Python Programming" in
 # Kapitel 5.
 #
 setuptools.setup(
     name="eapi",
     url="https://github.com/tbs1-bo/ea_rpi_modul",
-    version= eapi.VERSION,
+    version=eapi.VERSION,
     description="Modul zur Ansteuerung eines EA-Moduls fuer den Raspberry Pi.",
     long_description="Ein Modul zu Ansteuerung eines Eingabe-Ausgabe-Moduls fuer den Raspberry. Fuer die Ausgabe dienen LEDs und fuer die Eingabe Taster. Es kommt im Bildungsbereich zum Einsatz.",
     author="Marco Bakera",
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['eapi.unittests']),
     test_suite="eapi.unittests",
     classifiers=[  # gemäß https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Education",
         "Natural Language :: German",
         "Programming Language :: Python :: 3 :: Only",
@@ -36,7 +36,8 @@ setuptools.setup(
 # twine upload dist/*
 
 # Damit der Upload klappt, muss eine Datei ~/.pypirc mit Zugangsdaten für ein
-# Account bei PyPi vorhanden sein. Der Inhalt der Datei könnte wie folgt aussehen:
+# Account bei PyPi vorhanden sein. Der Inhalt der Datei könnte wie folgt
+# aussehen:
 #
 # [distutils]
 # index-servers=pypi
@@ -45,4 +46,3 @@ setuptools.setup(
 # repository = https://upload.pypi.io/legacy/
 # username = mein_username
 # password = mein_passwort
-
